9213d53fe1d9802b23e8d496962a5b72d74e007d
jogit99 <145586383+jogit99@users.noreply.github.com>
2024-05-02 21:44:13 +0700

    added the Github repo URl

--- src/App.js
+++ src/App.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jo and is{" "}
+        <a href="https://github.com/jogit99/react-weather-app" target="blank">
+          open sourced on Github"
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-
