0f8c1d009a45d95e4dfdcfa0d3785853493ec699
jogit99 <145586383+jogit99@users.noreply.github.com>
2024-05-04 13:07:51 +0700

    added Weather js and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="containter">
-        <h1>Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Jo and is{" "}
           <a href="https://github.com/jogit99/react-weather-app" target="blank">
