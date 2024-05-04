d3e0ab7d9fa8f8492a9840aa75a99434f6730039
jogit99 <145586383+jogit99@users.noreply.github.com>
2024-05-04 11:24:12 +0700

    added bootstrap to npm

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jo and is{" "}
-        <a href="https://github.com/jogit99/react-weather-app" target="blank">
-          open sourced on Github"
-        </a>
-      </footer>
+      <div className="containter">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Jo and is{" "}
+          <a href="https://github.com/jogit99/react-weather-app" target="blank">
+            open sourced on Github"
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
